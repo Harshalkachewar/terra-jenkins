f7d68e8d4d1445d15aaefd4042847f1e52970f19
Harshalkachewar <111994225+Harshalkachewar@users.noreply.github.com>
2025-03-26 11:50:15 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    linux = {
+      source = "TelkomIndonesia/linux"
+      version = "0.7.0"
+    }
+  }
+}
+
+provider "linux" {
+    host     = "192.168.180.129"
+    port     = 22
+    user     = "harshal"
+    password = "redhat"
+}
+
+resource "linux_file" "unnatifile" {
+    path = "/tmp/testdir"
+}
