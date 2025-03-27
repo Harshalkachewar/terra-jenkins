966f1a4ad05cfad1a9cb08e7e540202566dfa623
Harshalkachewar <111994225+Harshalkachewar@users.noreply.github.com>
2025-03-27 17:24:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,26 +1,58 @@
 terraform {
   required_providers {
     linux = {
-      source = "TelkomIndonesia/linux"
+      source  = "TelkomIndonesia/linux"
       version = "0.7.0"
     }
   }
 }
 
 provider "linux" {
-    host     = "192.168.180.129"
-    port     = 22
-    user     = "harshal"
-    password = "redhat"
+  host     = "192.168.180.129"
+  port     = 22
+  user     = "harshal"
+  password = "redhat"
 }
 
-resource "linux_directory" "resource1" {
-    path = "/tmp/terraform1"
+# Define variables for paths
+variable "directory_path" {
+  default = "/tmp/terraform1"
 }
 
-resource "linux_file" "resource2" {
-    path = "/tmp/terraform1/unnati.txt"
-    content = <<-EOF
-        hello world
-    EOF
+variable "file_path" {
+  default = "/tmp/terraform1/unnati.txt"
+}
+
+# Create directory using null_resource
+resource "null_resource" "create_directory" {
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      host     = "192.168.180.129"
+      user     = "harshal"
+      password = "redhat"
+    }
+
+    inline = [
+      "mkdir -p ${var.directory_path}"
+    ]
+  }
+}
+
+# Create file with content
+resource "null_resource" "create_file" {
+  depends_on = [null_resource.create_directory]
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      host     = "192.168.180.129"
+      user     = "harshal"
+      password = "redhat"
+    }
+
+    inline = [
+      "echo 'hello world' > ${var.file_path}"
+    ]
+  }
 }
